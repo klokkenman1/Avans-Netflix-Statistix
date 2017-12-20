38be98298512ca3e3840e4caf6e80c951df766d3
Kelvin Snepvangers <kelvinsnepvangers@gmail.com>
2017-12-20 09:49:04 +0100

    Started work on the database connection

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,18 @@
+import javafx.util.Pair;
+
 import javax.swing.SwingUtilities;
+import java.sql.ResultSet;
 
 public class Main {
 
-    public static SQLHelper helper;
+    private static SQLHelper helper;
 
     public static void main(String[] args) {
         helper = new SQLHelper("jdbc:sqlserver://localhost\\SQLEXPRESS;databaseName=Test;integratedSecurity=true;");
-        helper.Create("Persoon","Fay", "Snepvangers");
-        UserInterface ui = new UserInterface();
-        SwingUtilities.invokeLater(ui);
+        helper.Create("Persoon","Kelvin", "Snepvangers");
+        System.out.println(helper.Read("Persoon",new Pair("Achternaam","Snepvangers")));
+
+        //UserInterface ui = new UserInterface();
+        //SwingUtilities.invokeLater(ui);
     }
 }
--- src/SQLHelper.java
+++ src/SQLHelper.java
@@ -1,39 +1,45 @@
-import java.sql.*;
+import javafx.util.Pair;
 
-import static java.lang.System.in;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class SQLHelper {
-    private static Connection connection;
+    private String connectionString;
 
     public SQLHelper(String connectionString){
         try {
             // 'Importeer' de driver
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
             // Maak de verbinding met de database.
-            connection = DriverManager.getConnection(connectionString);
+            this.connectionString = connectionString;
         }
         catch (Exception e) {
-            System.out.println(e);
+            e.printStackTrace();
         }
     }
 
     public void Create(String table, Object... values){
+        Connection connection = null;
         PreparedStatement stmt = null;
         try {
+            connection = DriverManager.getConnection(connectionString);
             // Stel een SQL query samen.
-            String SQL = "INSERT INTO " + table + " VALUES (";
+            StringBuilder SQL = new StringBuilder("INSERT INTO " + table + " VALUES (");
             for (int i = 0; i < values.length; i++)
             {
                 if (i != values.length - 1)
                 {
-                    SQL += "?, ";
+                    SQL.append("?, ");
                 }
                 else
                 {
-                    SQL += "?)";
+                    SQL.append("?)");
                 }
             }
-            stmt = connection.prepareStatement(SQL);;
+            stmt = connection.prepareStatement(SQL.toString());
             for (int i = 0; i < values.length; i++)
             {
                 stmt.setString(i + 1, values[i].toString());
@@ -47,30 +53,59 @@ public class SQLHelper {
             e.printStackTrace();
         }
         finally {
-            if (stmt != null) try { stmt.close(); } catch(Exception e) {}
-            if (connection != null) try { connection.close(); } catch(Exception e) {}
+            if (stmt != null) try { stmt.close(); } catch(Exception ignored) {}
+            if (connection != null) try { connection.close(); } catch(Exception ignored) {}
         }
     }
 
-    public ResultSet Read(String table){
-        Statement stmt = null;
+    public List<Map<String, Object>>  Read(String table, Pair<String,Object>... cells){
+        Connection connection = null;
+        PreparedStatement stmt = null;
         ResultSet rs = null;
+        List<Map<String, Object>> result = null;
         try {
+            connection = DriverManager.getConnection(connectionString);
             // Stel een SQL query samen.
-            String SQL = "SELECT * FROM " + table;
-            stmt = connection.createStatement();
-            // Voer de query uit op de database.
-            rs = stmt.executeQuery(SQL);
+            StringBuilder SQL = new StringBuilder("SELECT * FROM " + table + " WHERE");
+            for (Pair cell : cells)
+            {
+                SQL.append(" ").append(cell.getKey()).append(" = ? AND");
+            }
+            SQL.delete(SQL.length()-3,SQL.length());
+
+            stmt = connection.prepareStatement(SQL.toString());
+
+            for (int i = 0; i < cells.length; i++)
+            {
+                stmt.setString(i + 1, cells[i].getValue().toString());
+            }
+
+            rs = stmt.executeQuery();
+            result = resultSetToList(rs);
         }
         // Handle any errors that may have occurred.
         catch (Exception e) {
             e.printStackTrace();
         }
         finally {
-            if (rs != null) try { rs.close(); } catch(Exception e) {}
-            if (stmt != null) try { stmt.close(); } catch(Exception e) {}
-            if (connection != null) try { connection.close(); } catch(Exception e) {}
+            if (rs != null) try { rs.close(); } catch(Exception ignored) {}
+            if (stmt != null) try { stmt.close(); } catch(Exception ignored) {}
+            if (connection != null) try { connection.close(); } catch(Exception ignored) {}
         }
-        return rs;
+        return result;
+    }
+
+    private List<Map<String, Object>> resultSetToList(ResultSet rs) throws SQLException {
+        ResultSetMetaData md = rs.getMetaData();
+        int columns = md.getColumnCount();
+        List<Map<String, Object>> rows = new ArrayList<>();
+        while (rs.next()){
+            Map<String, Object> row = new HashMap<>(columns);
+            for(int i = 1; i <= columns; ++i){
+                row.put(md.getColumnName(i), rs.getObject(i));
+            }
+            rows.add(row);
+        }
+        return rows;
     }
 }
