8dbcad20a3e3feedd5c7c49bfd34d73c3454094e
Kelvin Snepvangers <kelvinsnepvangers@gmail.com>
2018-01-13 18:38:43 +0100

    Revert "Revert "Merge branch 'master' of https://github.com/klokkenman1/Avans-Netflix-Statistix""
    This reverts commit b04415f56a7af328db303245226dea7666ce155e.

---
+++ src/Panels/MoviePanel.java
@@ -0,0 +1,24 @@
+package Panels;
+
+import Helpers.SQLHelper;
+
+import javax.swing.*;
+import java.util.List;
+import java.util.Map;
+
+public class MoviePanel extends JPanel{
+    public MoviePanel(){
+        setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
+        List<Map<String, Object>> result=  SQLHelper.executeQuery(
+                "SELECT TOP 1 Programma.Titel,Tijdsduur\n" +
+                        "FROM Programma JOIN Film ON Film.FilmID = Programma.ProgrammaID\n" +
+                        "WHERE Leeftijdsindicatie < 16\n" +
+                        "ORDER BY Tijdsduur DESC;");
+        add(new JLabel("Hier staat de langste film voor onder de 16"));
+        for (Map<String, Object> row : result)
+            add(new JLabel(row.get("Titel").toString()+"  " + row.get("Tijdsduur") ));
+        System.out.println(result);
+        // op deze pagina zie je de film met de langste tijdsduur met een leeftijdsindicatie onder de 16
+
+    }
+}
