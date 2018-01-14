777b8fffe7c38a97a9a277d1b041d9d30fb3718f
Kelvin Snepvangers <kelvinsnepvangers@gmail.com>
2018-01-14 16:26:17 +0100

    Added the data script, changed database

--- src/Panels/AccountMoviePanel.java
+++ src/Panels/AccountMoviePanel.java
@@ -1,48 +1,60 @@
 package Panels;
 
+import Helpers.SQLHelper;
+
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.List;
+import java.util.Map;
 
-public class AccountMoviePanel extends JPanel {
+public class AccountMoviePanel extends JPanel implements ActionListener {
 
-    //Creating all the "locked" labels
-    String sql = "select profielnaam from profile";
-    private JLabel Account = new JLabel("Account Name:");
-    private JLabel Account2 = new JLabel(sql);
-    private JLabel Test = new JLabel("RandomInfo:");
-
-    //Creating all the "Variable" labels
-    public static JLabel ALabel = new JLabel();
-
-    public void Account2() throws ClassNotFoundException {
-
-        try {
-
-            Connection con = DriverManager.getConnection("jdbc:mysql://localhost/netflix", "root", "");
-            String sql = "select profielnaam from profiel";
-            System.out.println("Connected to Database");
-            Statement statement = con.createStatement();
-            ResultSet result = statement.executeQuery(sql);
-            while (result.next()) {
-                String profielnaam = result.getString(1);
-                System.out.println(profielnaam);
-            }
-        } catch (Exception ex) {
-            System.out.println("Error");
-        }
-    }
+    private List<Map<String, Object>> accounts;
+    private JPanel resultPanel;
 
     public AccountMoviePanel() {
-        add(Account);
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+        JComboBox seriesSelector = new JComboBox();
+        seriesSelector.setMaximumSize(new Dimension(175,25));
 
-        add(Account2);
+        accounts = SQLHelper.read("Account");
+        System.out.println(accounts.size());
+        seriesSelector.addItem("");
+        for (Map<String, Object> row : accounts)
+            seriesSelector.addItem(row.get("Naam"));
 
-        add(ALabel);
+        add(new JLabel("Hier word voor een geselecteerde serie per aflevering het gemiddeld bekeken % van de tijdsduur weergegeven (Als een aflevering nooit is bekeken staat deze niet in de lijst)"));
+        add(new JLabel("Selecteer een Serie:"));
+        seriesSelector.addActionListener(this);
+        seriesSelector.setAlignmentX(Component.LEFT_ALIGNMENT);
+        add(seriesSelector);
+        resultPanel = new JPanel();
+        resultPanel.setLayout(new BoxLayout(resultPanel, BoxLayout.Y_AXIS));
+        add(resultPanel);
+    }
 
-        add(Test);
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        JComboBox cb = (JComboBox)e.getSource();
+        if (cb.getSelectedIndex() != 0){
+            Map<String, Object> selectedSeries = accounts.get(cb.getSelectedIndex() - 1);
+            List<Map<String, Object>> result = SQLHelper.executeQuery("SELECT Programma.ProgrammaID, Programma.Titel, AVG(Bekeken.Percentage) AS AvgPerc\n" +
+                    "FROM Bekeken\n" +
+                    "JOIN Aflevering ON Bekeken.Gezien = Aflevering.AfleveringID\n" +
+                    "JOIN Programma ON Aflevering.AfleveringID = Programma.ProgrammaID\n" +
+                    "WHERE Aflevering.Serie = " + selectedSeries.get("SerieID") + "\n" +
+                    "GROUP BY Programma.ProgrammaID, Programma.Titel;");
 
+            resultPanel.removeAll();
+            for (Map<String, Object> row : result)
+                resultPanel.add(new JLabel("Volgnummer: " + row.get("ProgrammaID") + " Titel: " + row.get("Titel") + " Gemiddeld bekeken % van tijdsduur: " + row.get("AvgPerc") + "%"));
+            resultPanel.updateUI();
+        }
     }
 }
